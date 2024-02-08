e38cd609e51c7a0cfc18858d860dec5a2e3c0020
Juliano Quites <juliano.quites@gmail.com>
2024-02-08 16:09:00 -0300

    Fixed and added more functionality

--- rps_game.py
+++ rps_game.py
@@ -1,34 +1,49 @@
 import random
+import sys
 from enum import Enum
 
-class RPS(Enum):
-  ROCK = "1"
-  PAPER = "2"
-  SCISSORS = "3"
+def play_rps():
+  class RPS(Enum):
+    ROCK = "1"
+    PAPER = "2"
+    SCISSORS = "3"
 
-playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors: \n\n")
-valid_inputs = ["1","2","3"]
-
-while playerchoice not in valid_inputs:
-  print("You must type 1, 2 or 3.\n")
   playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors: \n\n")
-      
-computerchoice = random.choice("123")
+  valid_inputs = ["1","2","3"]
+  computerchoice = random.choice("123")
 
-print("")
-print("You chose " + str(RPS(playerchoice)).replace('RPS.','') + ".")
-print("Python chose " + str(RPS(computerchoice)).replace('RPS.','') + ".")
-print("")
+  if playerchoice not in valid_inputs:
+    print("You must type 1, 2 or 3.\n")
+    return play_rps()
+        
+  print("\nYou chose " + str(RPS(playerchoice)).replace('RPS.','') + ".")
+  print("Python chose " + str(RPS(computerchoice)).replace('RPS.','') + ".\n")
 
-if playerchoice == "1" and computerchoice == "3":
-  print("🎉 You win!")
-elif playerchoice == "2" and computerchoice == "1":
-  print("🎉 You win!")
-elif playerchoice == "3" and computerchoice == "2":
-  print("🎉 You win!")
-elif playerchoice == computerchoice:
-  print("😲 Its a tie!")
-else:
-  print("🐍 Python wins!")
+  if playerchoice == "1" and computerchoice == "3":
+    print("🎉 You win!")
+  elif playerchoice == "2" and computerchoice == "1":
+    print("🎉 You win!")
+  elif playerchoice == "3" and computerchoice == "2":
+    print("🎉 You win!")
+  elif playerchoice == computerchoice:
+    print("😲 Its a tie!")
+  else:
+    print("🐍 Python wins!")
+  
+  print("\n Play again?")
 
+  while True:
+    playagain = input("\nY for Yes or \nN for No\n")
+    if playagain.lower() not in ["y","n"]:
+      continue
+    else:
+      break
 
+  if playagain.lower() == "y":
+    return play_rps()
+  else:
+    print("\n🎉🎉🎉🎉")
+    print("Thank you for playing!\n")
+    sys.exit("Bye! 👋")
+
+play_rps()
